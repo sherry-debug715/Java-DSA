d39e9f32fa31c23bedb0cfa8e5b3f105582553ab
sherryyu <bambooyu715@gmail.com>
2024-04-16 14:59:27 -0400

    update notes for java - binary tree: problem zigzag level order traversal

---
+++ BinaryTree/MinDepth.java
@@ -0,0 +1,22 @@
+package BinaryTree;
+
+class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+public class MinDepth {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (left == 0 || right == 0) {
+            return left + right + 1;
+        } 
+        return 1 + Math.min(left, right);
+    }
+}
