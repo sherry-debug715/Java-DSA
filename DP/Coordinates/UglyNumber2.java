503b0b9384705125eaa5562eaf0a06028f1f7907
sherryyu <bambooyu715@gmail.com>
2024-09-18 10:21:58 -0400

    update adjusted solution for problem UglyNumber2

--- DP/Coordinates/UglyNumber2.java
+++ DP/Coordinates/UglyNumber2.java
@@ -1,31 +1,42 @@
 package DP.Coordinates;
-// Time: O(N)
-// Space: O(N)
-public class UglyNumber2 {
-    public int nthUglyNumber(int n) {
-        // dp[i] is the next smallest ugly number of dp[i - 1]
-        long[] dp = new long[n];
-        dp[0] = 1L;
-        // each pointers epresent the factors of each prime number 
-        int l2 = 0, l3 = 0, l5 = 0;
 
+public class UglyNumber2 {
+     // Time: O(n) because we process each number once 
+     // Space: O(n) to store the array of ugly numbers 
+     public int nthUglyNumber(int n) {
+        long[] dp = new long[n];
+         // The first ugly number is defined as 1L
+        dp[0] = 1L;
+
+        // Pointers representing the current positions in the dp array for multiples of 2, 3, and 5.
+        int l2 = 0, l3 = 0, l5 = 0; 
+
+        // Generate ugly numbers up to the nth position 
         for (int i = 1; i < n; i++) {
-            long two = dp[l2] * 2;
-            long three = dp[l3] * 3;
-            long five = dp[l5] * 5;
-            // conpute the smallest ugly 
-            long min = Math.min(two, Math.min(three, five));
-            dp[i] = min;
-            if (min == two) {
+            long num2 = dp[l2] * 2;
+            long num3 = dp[l3] * 3;
+            long num5 = dp[l5] * 5;
+
+            // Find the smallest next ugly number among the factors of 2, 3, and 5.
+            long nextUgly = Math.min(num2, Math.min(num3, num5));
+
+            // update dp with the smallest next ugly number.
+            dp[i] = nextUgly;
+            
+            // Move pointers forward to avoid duplicates.
+            // If the next ugly number is a multiples of 2, 3, or 5, move the respective pointer.
+            if (nextUgly == num2) {
                 l2 += 1;
             }
-            if (min == three) {
+            if (nextUgly == num3) {
                 l3 += 1;
             }
-            if (min == five) {
+            if (nextUgly == num5) {
                 l5 += 1;
             }
-        } 
-        return (int) dp[dp.length - 1];
+        }
+
+        // return the nth ugly number (0-indexed)
+        return (int) dp[n - 1];
     }
 }
