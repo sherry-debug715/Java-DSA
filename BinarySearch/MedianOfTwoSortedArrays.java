0da60793ba6706b784fb5b751dc7085dc4dc6d14
sherryyu <bambooyu715@gmail.com>
2024-09-20 10:46:13 -0400

    update solution problem median of two sorted arrays

--- BinarySearch/MedianOfTwoSortedArrays.java
+++ BinarySearch/MedianOfTwoSortedArrays.java
@@ -1,33 +1,38 @@
 package BinarySearch;
-// time: O(logN)
-// Space: O(1)
 public class MedianOfTwoSortedArrays {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-        if (a.length == 0 && b.length == 0) {
-            return 0.0;
+    // Time: O(log(sizeA + sizeB))
+    // Space: O(1)
+    public double findMedianSortedArrays(int[] shorter, int[] longer) {
+        int aSize = shorter.length, bSize = longer.length;
+        // ensures a is the shorter array.
+        if (aSize > bSize) {
+            return findMedianSortedArrays(longer, shorter);
         }
 
-        int total = a.length + b.length; 
-        boolean isEven = total % 2 == 0; 
-        int p1 = 0, p2 = 0, L = 0, R =0;
-        int half = total / 2;
+        int half = (aSize + bSize) / 2;
+        boolean isEven = (aSize + bSize) % 2 == 0;
+        int left = 0, right = shorter.length - 1; 
+        while (true) {
+            // p1 is pointer for longer, p2 is pointer for shorter
+            int p2 = left + (right - left) / 2;
+            int p1 = half - p2;
 
-        // a, b are both sorted, only need to find the half th number 
-        for (int i = 0; i < half + 1; i++) {
-            // always move left pointer before right pointer is moved 
-            L = R;
-            if (p2 == b.length || p1 < a.length && a[p1] <= b[p2]) {
-                R = a[p1];
-                p1 += 1;
+            // Edge cases: use MIN and MAX values when partition is out of bounds.
+            int leftL = p1 > 0 ? longer[p1 - 1] : Integer.MIN_VALUE;
+            int rightL = p1< longer.length ? longer[p1] : Integer.MAX_VALUE;
+            int leftS = p2 > 0 ? shorter[p2 - 1] : Integer.MIN_VALUE;
+            int rightS = p2 < shorter.length ? shorter[p2] : Integer.MAX_VALUE;
+
+            if (leftL <= rightS && leftS <= rightL) {
+                if (isEven) {
+                    return (double) (Math.max(leftL, leftS) + Math.min(rightL, rightS)) / 2;
+                }
+                return (double) Math.min(rightL, rightS);
+            } else if (leftL > rightS) {
+                left = p2 + 1;
             } else {
-                R = b[p2];
-                p2 += 1;
+                right = p2 - 1;
             }
         }
-
-        if (isEven) {
-            return (L + R) / 2.0;
-        }
-        return R;
     }
 }
